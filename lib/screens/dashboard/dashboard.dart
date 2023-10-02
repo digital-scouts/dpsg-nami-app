c1872ca0a40df2476ec99cb77497f2c520a53d4f
Janneck Lange <langejanneck@gmail.com>
2023-10-02 23:17:58 +0200

    dart format

--- lib/screens/dashboard/dashboard.dart
+++ lib/screens/dashboard/dashboard.dart
@@ -9,7 +9,7 @@ class Dashboard extends StatefulWidget {
 
 class _DashboardState extends State<Dashboard> {
   @override
-    Widget build(BuildContext context) {
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Center(child: Text('Dashboard')),
--- lib/utilities/hive/settings.dart
+++ lib/utilities/hive/settings.dart
@@ -1,4 +1,5 @@
 import 'package:hive/hive.dart';
+
 // flutter packages pub run build_runner build
 enum SettingValue {
   namiApiCookie,
