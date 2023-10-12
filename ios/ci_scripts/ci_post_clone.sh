02c24b03fa15619a97405297bc4e9c4fabfd15ff
Janneck Lange <langejanneck@gmail.com>
2023-10-12 10:54:19 +0200

    sudo problem

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
+echo 'export GEM_HOME=$HOME/gems' >>~/.bash_profile
+echo 'export PATH=$HOME/gems/bin:$PATH' >>~/.bash_profile
+export GEM_HOME=$HOME/gems
+export PATH="$GEM_HOME/bin:$PATH"
 
 # Install CocoaPods using Homebrew.
 brew install cocoapods
 
 # Install Flutter
-brew install --cask --no-sandbox flutter
+brew install --cask flutter
 
 # Run Flutter doctor
 flutter doctor
