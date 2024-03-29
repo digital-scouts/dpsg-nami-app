09676aa524651837b253a382e1d682fea23b6fde
Janneck Lange <dev@jannecklange.de>
2024-03-29 12:11:06 +0100

    update script

--- ios/ci_scripts/ci_pre_xcodebuild.sh
+++ ios/ci_scripts/ci_pre_xcodebuild.sh
@@ -12,14 +12,14 @@ if [ -z "$WIREDASH_PROJECT_ID" ]; then
     exit 1
 fi
 # Überprüfen, ob die .env-Datei existiert
-cd $CI_WORKSPACE/ci_scripts || exit 1
-if [ ! -f ../../.env ]; then
+cd $CI_PRIMARY_REPOSITORY_PATH || exit 1
+if [ ! -f .env ]; then
    exit 1
 fi
 
 # Schreiben der Umgebungsvariablen in die .env-Datei
-echo "WIREDASH_SECRET=$WIREDASH_SECRET" > ../../.env
-echo "WIREDASH_PROJECT_ID=$WIREDASH_PROJECT_ID" >> ../../.env
+echo "WIREDASH_SECRET=$WIREDASH_SECRET" > .env
+echo "WIREDASH_PROJECT_ID=$WIREDASH_PROJECT_ID" >> .env
 echo "WIREDASH_SECRET=$WIREDASH_SECRET | WIREDASH_PROJECT_ID=$WIREDASH_PROJECT_ID"
 
 echo "Die Umgebungsvariablen wurden erfolgreich in die .env-Datei geschrieben."
