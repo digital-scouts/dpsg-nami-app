077b84be4093e16cf69aa55b5e3eb42ff0d1d255
Janneck Lange <dev@jannecklange.de>
2024-05-06 12:25:22 +0200

    show and load führungszeugnis dokumente

--- lib/screens/profil/profil.dart
+++ lib/screens/profil/profil.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:nami/utilities/nami/model/nami_fz.model.dart';
+import 'package:nami/utilities/nami/nami_fz.service.dart';
 import 'package:nami/utilities/nami/nami_rechte.dart';
+import 'package:open_file/open_file.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
 
 class Profil extends StatefulWidget {
   const Profil({Key? key}) : super(key: key);
@@ -9,6 +14,56 @@ class Profil extends StatefulWidget {
 }
 
 class _ProfilState extends State<Profil> {
+  void loadAntrag() {
+    loadFzAntrag().then((pdfData) async {
+      final output = await getTemporaryDirectory();
+      final file = File("${output.path}/fuehrungszeugnisAntrag.pdf");
+      await file.writeAsBytes(pdfData, flush: true);
+
+      OpenFile.open(file.path);
+    });
+  }
+
+  Widget buildFzList() {
+    return FutureBuilder(
+      future: loadFzDocumenets(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        }
+
+        if (snapshot.hasError) {
+          return const Center(child: Text("Fehler beim Laden"));
+        }
+
+        final documents = snapshot.data as List<FzDocument>;
+        documents.sort((a, b) => b.erstelltAm.compareTo(a.erstelltAm));
+
+        return ListView.builder(
+          shrinkWrap: true,
+          itemCount: documents.length,
+          itemBuilder: (context, index) {
+            final document = documents[index];
+            return ListTile(
+              title: Text(
+                  'Bescheinigung (${document.fzNummer}) - ${document.erstelltAm.month}/${document.erstelltAm.year}'),
+              onTap: () {
+                loadFzDocument(document.id).then((pdfData) async {
+                  final output = await getTemporaryDirectory();
+                  final file = File(
+                      "${output.path}/dpsg-fz-bescheinigung_${document.fzNummer}.pdf");
+                  await file.writeAsBytes(pdfData, flush: true);
+
+                  OpenFile.open(file.path);
+                });
+              },
+            );
+          },
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,7 +86,12 @@ class _ProfilState extends State<Profil> {
                   )
               ],
             ),
-          )
+          ),
+          Text("Führungszeugnis",
+              style: Theme.of(context).textTheme.titleMedium),
+          TextButton(
+              onPressed: loadAntrag, child: const Text("Antragsunterlagen")),
+          buildFzList()
         ],
       ),
     );
