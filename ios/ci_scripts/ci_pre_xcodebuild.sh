3dfb3dea859fd77cdcb9936dcdaa4bc3e83bfa29
Janneck Lange <langejanneck@gmail.com>
2024-09-12 17:37:48 +0200

    ci_pre_xcodebuild.sh aktualisieren

--- ios/ci_scripts/ci_pre_xcodebuild.sh
+++ ios/ci_scripts/ci_pre_xcodebuild.sh
@@ -11,6 +11,11 @@ if [ -z "$WIREDASH_PROJECT_ID" ]; then
     echo "Fehler: WIREDASH_PROJECT_ID ist nicht gesetzt."
     exit 1
 fi
+if [ -z "$GEOAPIFY_KEY" ]; then
+    echo "Fehler: GEOAPIFY_KEY ist nicht gesetzt."
+    exit 1
+fi
+
 # Überprüfen, ob die .env-Datei existiert
 cd $CI_PRIMARY_REPOSITORY_PATH || exit 1
 if [ ! -f .env ]; then
@@ -20,7 +25,8 @@ fi
 # Schreiben der Umgebungsvariablen in die .env-Datei
 echo "WIREDASH_SECRET=$WIREDASH_SECRET" > .env
 echo "WIREDASH_PROJECT_ID=$WIREDASH_PROJECT_ID" >> .env
-echo "WIREDASH_SECRET=$WIREDASH_SECRET | WIREDASH_PROJECT_ID=$WIREDASH_PROJECT_ID"
+echo "GEOAPIFY_KEY=$GEOAPIFY_KEY" >> .env
+echo "WIREDASH_SECRET=$WIREDASH_SECRET | WIREDASH_PROJECT_ID=$WIREDASH_PROJECT_ID | GEOAPIFY_KEY=$GEOAPIFY_KEY"
 
 echo "Die Umgebungsvariablen wurden erfolgreich in die .env-Datei geschrieben."
 
